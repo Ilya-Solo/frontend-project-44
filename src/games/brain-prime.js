25132f4cf7fb6a278179d97b52fd60c208e6ea77
Daniel Leroy <57351061+AniWaffl@users.noreply.github.com>
2023-06-13 21:39:42 +0200

    Update brain-prime.js
    replace num is prime func. Is a MORE optimized

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -2,19 +2,11 @@ import getRandNumber, { booleanToString } from '../usefulFunctions.js';
 
 const gameTask = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const isPrime = (number) => {
-  if (number === 2) {
-    return true;
-  } if (number % 2 === 0 || number < 2) {
-    return false;
-  }
-
-  for (let i = Math.floor(number / 2); i > 1; i -= 2) {
-    if (number % i === 0) return false;
-    if (i % 2 === 0) i += 1;
-  }
-
-  return true;
+const isPrime = num => {
+    for(let i = 2, s = Math.sqrt(num); i <= s; i++) {
+        if(num % i === 0) return false;
+    }
+    return num > 1;
 };
 
 const generateQuestion = (maxNumberLimit = 100) => {
